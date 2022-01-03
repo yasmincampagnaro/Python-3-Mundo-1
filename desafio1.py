e14861fd30e3a593bf972d6b6c40b1fe8af532cc
Yasmin Campagnaro Soute <gabrielecampagnaro234@gmail.com>
2022-01-03 12:16:50 -0300

    Desafio 1

---
+++ desafio1.py
@@ -0,0 +1,5 @@
+""" "nome" é uma variável que recebe o resultado do input """
+nome=input('Qual seu nome?')
+
+"""Exibi na tela a mensagem concatenando (+) o resultado do "nome" """
+print('Olá', nome,'. É um prazer te conhecer')
