4835a54c3672bd8cc605286887652ecb40c2e88b
Yasmin Campagnaro Soute <gabrielecampagnaro234@gmail.com>
2022-01-03 13:20:08 -0300

    Sua data de nascimento

--- boasvindasDesafio1.py
+++ boasvindasDesafio1.py
@@ -1,5 +1,5 @@
 """ "nome" é uma variável que recebe o resultado do input """
 nome=input('Qual é seu nome?')
 
-""" Irá exibir na tela a mensagem "Prazer em te conhecer" + o resultado do "nome" """
-print('Prazer em te conhecer', nome)
+""" Irá exibir na tela a mensagem concatenando (+) o resultado do "nome" """
+print('Olá', nome,'. É um prazer te conhecer')
