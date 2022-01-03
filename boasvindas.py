ebd8cf830743804a28bf8dcbdc943c6dbd9c9c5a
Yasmin Campagnaro Soute <gabrielecampagnaro234@gmail.com>
2022-01-03 12:13:32 -0300

    Boas-vindas

---
+++ boasvindas.py
@@ -0,0 +1,5 @@
+""" "nome" é uma variável que recebe o resultado do input """
+nome=input('Qual é seu nome?')
+
+""" Irá exibir na tela a mensagem "Prazer em te conhecer" + o resultado do "nome" """
+print('Prazer em te conhecer', nome)
